6542e7e2bcd372cf734535162a15f384b53c6d86
Scott Dormand <sdormand@hotmail.com>
2020-03-24 09:27:45 +0000

    password post

--- server/routes/forgot-password.js
+++ server/routes/forgot-password.js
@@ -2,7 +2,24 @@ const find = require('../../lib/find')
 
 module.exports = {
   get: (req, res) => {
-    res.render('forgot-pasword.ejs', {
+    res.render('forgot-password.ejs', {
       flash: res.locals.flash })
+  },
+
+  post: async (req, res) => {
+    const email = req.body.email
+    const results = await find.findEmail(email)
+
+    if (results === null && results === undefined) {
+      req.session.flash = {
+        message: 'Invalid user name'
+      }
+      res.redirect('/forgot-password')
+    } else {
+      req.session.flash = { 
+        message: 'Email sent'
+      }   
+      res.redirect('/forgot-password')
+      }
+    }
   }
-}
