5ffb79ed678a0b01631e2784b854591848e58064
Scott Dormand <sdormand@hotmail.com>
2020-04-13 15:51:25 +0100

    menu links

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -1,4 +1,6 @@
 module.exports = {
   get: (req, res) => {
-    res.render('groups.ejs')
-  } }
+    res.render('groups.ejs', {
+      user: req.session.user.firstname + ' ' + req.session.user.surname
+    })
+  }}
