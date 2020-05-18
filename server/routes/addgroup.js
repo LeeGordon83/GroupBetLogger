cee377a7b52dee6b6312e06f5b98135b48bf8deb
Lee Gordon <lee.gordon@rpa.gsi.gov.uk>
2020-05-18 14:00:42 +0100

    addgroupredirect

--- server/routes/addgroup.js
+++ server/routes/addgroup.js
@@ -5,27 +5,29 @@ const users = []
 
 module.exports = {
   get: async (req, res) => {
-    const users = await find.findAll()
-    res.render('addgroup.ejs', { usersFound: users })
+    if (req.session.user !== undefined) {
+      const users = await find.findAll()
+      res.render('addgroup.ejs', { usersFound: users })
+    }
+    res.redirect('/login')
   },
 
   post: async (req, res) => {
-    console.log(req.body)
+    if (req.session.user !== undefined) {
+      const groupname = req.body.formdata[0].value
 
-    const groupname = req.body.formdata[0].value
+      const pickedArray = req.body.pickedArray
 
-    const pickedArray = req.body.pickedArray
+      await Promise.all(
+        pickedArray.map(async function (item) {
+          const user = await find.findUser(item)
+          users.push(user)
+        })
+      )
 
-    await Promise.all(
-      pickedArray.map(async function (item) {
-        const user = await find.findUser(item)
-        users.push(user)
-      })
-    )
-
-    await createGroupsWithUsers(groupname, users)
-
-    res.render('groups.ejs')
+      await createGroupsWithUsers(groupname, users)
+    }
+    res.redirect('/login')
   }
 
 }
