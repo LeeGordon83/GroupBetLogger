95ea1e7ece8003317ae18d74eff18416eb26f13b
Scott Dormand <sdormand@hotmail.com>
2020-05-27 13:04:11 +0100

    const

--- server/routes/group-overview.js
+++ server/routes/group-overview.js
@@ -3,7 +3,7 @@ const find = require('../lib/find')
 module.exports = {
   get: async (req, res) => {
     if (req.session.user !== undefined) {
-      group = await find.findGroup(req.params.id)
+      const group = await find.findGroup(req.params.id)
       res.render('group-overview.ejs', { groupFound : group.dataValues })
     }
     else{
