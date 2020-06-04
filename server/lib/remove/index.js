8245ce20d93e8dbcdae7b7281c4e9776ccc355bb
Scott Dormand <sdormand@hotmail.com>
2020-06-04 10:17:48 +0100

    remove-user-from-group

--- server/lib/remove/index.js
+++ server/lib/remove/index.js
@@ -1,7 +1,9 @@
 const { removeUsersFromGroup } = require('./remove-users-from-group')
 const { removeGroup } = require('./remove-group')
+const { removeUserFromGroup } = require('./remove-user-from-group')
 
 module.exports = {
   removeUsersFromGroup,
-  removeGroup
+  removeGroup,
+  removeUserFromGroup,
 }
